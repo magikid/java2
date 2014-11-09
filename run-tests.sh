196c032470cd7d3bdc6e60cf0f2c1236cc690052
Chris Jones <chris@christopherjones.us>
2014-11-09 13:19:02 -0500

    Still working on the build script

--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,18 @@
-cd "HW1"
-ant -f "HW1" -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test
-ant -f "HW1" -Dnb.internal.action.name=rebuild clean jar
+cd HW1
+echo "Running HW1 tests"
+ant -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test
+echo "Building HW1"
+ant -Dnb.internal.action.name=rebuild clean jar
 cd ..
 
-cd "p1"
-ant -f "p1" -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test
-ant -f "p1" -Dnb.internal.action.name=rebuild clean jar
+cd p1
+echo "Running p1 tests"
+ant -f p1 -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test
+echo "Building p1"
+ant -f p1 -Dnb.internal.action.name=rebuild clean jar
 cd ..
 
-cd "HW2"
-ant -f "HW2" -Dnb.internal.action.name=rebuild clean jar
+cd HW2
+echo "Building HW2"
+ant -f HW2 -Dnb.internal.action.name=rebuild clean jar
 cd ..
