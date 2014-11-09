843c5b29f8f13aa8da28e94fab703e4bd521ba83
Chris Jones <chris@christopherjones.us>
2014-11-09 14:11:27 -0500

    Added line endings

--- run-tests.sh
+++ run-tests.sh
@@ -1,26 +1,26 @@
-errors=0
-cd HW1
-echo "Running HW1 tests"
-ant -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test
-if [ "$?" -ne 0 ]; then errors=1; fi
-echo "Building HW1"
-ant -Dnb.internal.action.name=rebuild clean jar
-if [ "$?" -ne 0 ]; then errors=2; fi
-cd ..
+errors=0;
+cd HW1;
+echo "Running HW1 tests";
+ant -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test;
+if [ "$?" -ne 0 ]; then errors=1; fi;
+echo "Building HW1";
+ant -Dnb.internal.action.name=rebuild clean jar;
+if [ "$?" -ne 0 ]; then errors=2; fi;
+cd ..;
 
-cd p1
-echo "Running p1 tests"
-ant -f p1 -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test
-if [ "$?" -ne 0 ]; then errors=3; fi
-echo "Building p1"
-ant -f p1 -Dnb.internal.action.name=rebuild clean jar
-if [ "$?" -ne 0 ]; then errors=4; fi
-cd ..
+cd p1;
+echo "Running p1 tests";
+ant -f p1 -Dnb.internal.action.name=test -Dignore.failing.tests=true -Dnb.wait.for.caches=true test;
+if [ "$?" -ne 0 ]; then errors=3; fi;
+echo "Building p1";
+ant -f p1 -Dnb.internal.action.name=rebuild clean jar;
+if [ "$?" -ne 0 ]; then errors=4; fi;
+cd ..;
 
-cd HW2
-echo "Building HW2"
-ant -f HW2 -Dnb.internal.action.name=rebuild clean jar
+cd HW2;
+echo "Building HW2";
+ant -f HW2 -Dnb.internal.action.name=rebuild clean jar;
 if [ "$?" -ne 0 ]; then errors=5; fi
-cd ..
+cd ..;
 
-exit errors
+exit $errors;
