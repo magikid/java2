637ff520a8d9049c0385b7a72c24d67170eea9c1
Chris Jones <chris@christopherjones.us>
2014-11-11 14:24:03 -0500

    Made the view
    I followed the book for the most part from page 454-455.

--- HW3/src/hw3/AgeRecognitionView.java
+++ HW3/src/hw3/AgeRecognitionView.java
@@ -5,10 +5,38 @@
  */
 package hw3;
 
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+
 /**
  *
  * @author Chris W Jones <chris@christopherjones.us>
  */
-public class AgeRecognitionView {
+public class AgeRecognitionView extends JFrame {
     
+    public AgeRecognitionView(){
+        setLayout(new GridLayout(3,2,5,5));
+        
+        add(new JLabel("First Name"));
+        add(new JTextField());
+        add(new JLabel("Last name"));
+        add(new JTextField());
+        add(new JLabel("Age"));
+        add(new JTextField());
+        add(new JButton("Ok"));
+        
+    }
+    
+    public void createForm(AgeRecognition ar){
+        AgeRecognitionView frame = new AgeRecognitionView();
+        
+        frame.setTitle("HW3: Age Recognition");
+        frame.setSize(200, 125);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
